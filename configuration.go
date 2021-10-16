2e3523e9fe20c0aa99e933df59d80d2df235cf56
Alexey Kuzyashin <terr.kuzyashin@gmail.com>
2021-10-16 19:18:17 +0300

    Refactoring

--- configuration.go
+++ configuration.go
@@ -16,100 +16,102 @@ type ClientConfig struct {
 	// after receiving specified status code
 	RetryOnStatus []int
 
-	FastHTTPConfig struct {
-		// NoDefaultUserAgentHeader when set to true, causes the default
-		// User-Agent header to be excluded from the Request.
-		NoDefaultUserAgentHeader bool
+	FastHTTPConfig FastHTTPConfig
+}
 
-		// Attempt to connect to both ipv4 and ipv6 addresses if set to true.
-		//
-		// This option is used only if default TCP dialer is used,
-		// i.e. if Dial is blank.
-		//
-		// By default, client connects only to ipv4 addresses,
-		// since unfortunately ipv6 remains broken in many networks worldwide :)
-		DialDualStack bool
+type FastHTTPConfig struct {
+	// NoDefaultUserAgentHeader when set to true, causes the default
+	// User-Agent header to be excluded from the Request.
+	NoDefaultUserAgentHeader bool
 
-		// TLS config for https connections.
-		//
-		// Default TLS config is used if not set.
-		TLSConfig *tls.Config
+	// Attempt to connect to both ipv4 and ipv6 addresses if set to true.
+	//
+	// This option is used only if default TCP dialer is used,
+	// i.e. if Dial is blank.
+	//
+	// By default, client connects only to ipv4 addresses,
+	// since unfortunately ipv6 remains broken in many networks worldwide :)
+	DialDualStack bool
 
-		// Maximum number of connections per each host which may be established.
-		//
-		// DefaultMaxConnsPerHost is used if not set.
-		MaxConnsPerHost int
+	// TLS config for https connections.
+	//
+	// Default TLS config is used if not set.
+	TLSConfig *tls.Config
 
-		// Idle keep-alive connections are closed after this duration.
-		//
-		// By default, idle connections are closed
-		// after DefaultMaxIdleConnDuration.
-		MaxIdleConnDuration time.Duration
+	// Maximum number of connections per each host which may be established.
+	//
+	// DefaultMaxConnsPerHost is used if not set.
+	MaxConnsPerHost int
 
-		// Keep-alive connections are closed after this duration.
-		//
-		// By default, connection duration is unlimited.
-		MaxConnDuration time.Duration
+	// Idle keep-alive connections are closed after this duration.
+	//
+	// By default, idle connections are closed
+	// after DefaultMaxIdleConnDuration.
+	MaxIdleConnDuration time.Duration
 
-		// Per-connection buffer size for responses' reading.
-		// This also limits the maximum header size.
-		//
-		// Default buffer size is used if 0.
-		ReadBufferSize int
+	// Keep-alive connections are closed after this duration.
+	//
+	// By default, connection duration is unlimited.
+	MaxConnDuration time.Duration
 
-		// Per-connection buffer size for requests' writing.
-		//
-		// Default buffer size is used if 0.
-		WriteBufferSize int
+	// Per-connection buffer size for responses' reading.
+	// This also limits the maximum header size.
+	//
+	// Default buffer size is used if 0.
+	ReadBufferSize int
 
-		// Maximum duration for full response reading (including body).
-		//
-		// By default, response read timeout is unlimited.
-		ReadTimeout time.Duration
+	// Per-connection buffer size for requests' writing.
+	//
+	// Default buffer size is used if 0.
+	WriteBufferSize int
 
-		// Maximum duration for full request writing (including body).
-		//
-		// By default request write timeout is unlimited.
-		WriteTimeout time.Duration
+	// Maximum duration for full response reading (including body).
+	//
+	// By default, response read timeout is unlimited.
+	ReadTimeout time.Duration
 
-		// Maximum response body size.
-		//
-		// The client returns ErrBodyTooLarge if this limit is greater than 0
-		// and response body is greater than the limit.
-		//
-		// By default, response body size is unlimited.
-		MaxResponseBodySize int
+	// Maximum duration for full request writing (including body).
+	//
+	// By default request write timeout is unlimited.
+	WriteTimeout time.Duration
 
-		// Header names are passed as-is without normalization
-		// if this option is set.
-		//
-		// Disabled header names' normalization may be useful only for proxying
-		// responses to other clients expecting case-sensitive
-		// header names. See https://github.com/valyala/fasthttp/issues/57
-		// for details.
-		//
-		// By default, request and response header names are normalized, i.e.
-		// The first letter and the first letters following dashes
-		// are uppercased, while all the other letters are lowercased.
-		// Examples:
-		//
-		//     * HOST -> Host
-		//     * content-type -> Content-Type
-		//     * cONTENT-lenGTH -> Content-Length
-		DisableHeaderNamesNormalizing bool
+	// Maximum response body size.
+	//
+	// The client returns ErrBodyTooLarge if this limit is greater than 0
+	// and response body is greater than the limit.
+	//
+	// By default, response body size is unlimited.
+	MaxResponseBodySize int
 
-		// Path values are sent as-is without normalization
-		//
-		// Disabled path normalization may be useful for proxying incoming requests
-		// to servers that are expecting paths to be forwarded as-is.
-		//
-		// By default, path values are normalized, i.e.
-		// extra slashes are removed, special characters are encoded.
-		DisablePathNormalizing bool
+	// Header names are passed as-is without normalization
+	// if this option is set.
+	//
+	// Disabled header names' normalization may be useful only for proxying
+	// responses to other clients expecting case-sensitive
+	// header names. See https://github.com/valyala/fasthttp/issues/57
+	// for details.
+	//
+	// By default, request and response header names are normalized, i.e.
+	// The first letter and the first letters following dashes
+	// are uppercased, while all the other letters are lowercased.
+	// Examples:
+	//
+	//     * HOST -> Host
+	//     * content-type -> Content-Type
+	//     * cONTENT-lenGTH -> Content-Length
+	DisableHeaderNamesNormalizing bool
 
-		// Maximum duration for waiting for a free connection.
-		//
-		// By default, will not waiting, return ErrNoFreeConns immediately
-		MaxConnWaitTimeout time.Duration
-	}
+	// Path values are sent as-is without normalization
+	//
+	// Disabled path normalization may be useful for proxying incoming requests
+	// to servers that are expecting paths to be forwarded as-is.
+	//
+	// By default, path values are normalized, i.e.
+	// extra slashes are removed, special characters are encoded.
+	DisablePathNormalizing bool
+
+	// Maximum duration for waiting for a free connection.
+	//
+	// By default, will not waiting, return ErrNoFreeConns immediately
+	MaxConnWaitTimeout time.Duration
 }
